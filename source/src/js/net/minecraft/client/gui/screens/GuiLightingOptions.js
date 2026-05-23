ea911497510b549c2b4cd0330a9e03a1946ba7f8
Duy <duylw25@gmail.com>
2026-05-24 03:54:14 +0700

    Lighting Setting Folder Re-Organize

--- source/src/js/net/minecraft/client/gui/screens/GuiLightingOptions.js
+++ source/src/js/net/minecraft/client/gui/screens/GuiLightingOptions.js
@@ -7,11 +7,16 @@ export default class GuiLightingOptions extends GuiScreen {
     constructor(previousScreen) {
         super();
         this.previousScreen = previousScreen;
+        this.currentTab = "GLOBAL";
     }
 
     init() {
         super.init();
+        this.rebuildButtons();
+    }
 
+    rebuildButtons() {
+        this.buttonList = [];
         let settings = this.minecraft.settings;
         let renderer = this.minecraft.worldRenderer;
 
@@ -26,86 +31,111 @@ export default class GuiLightingOptions extends GuiScreen {
 
         let leftX = this.width / 2 - 155;
         let rightX = this.width / 2 + 5;
+        let centerX = this.width / 2 - 100;
 
-        // --- Column 1 (Left: General & Sun) ---
-        this.buttonList.push(new GuiSwitchButton("Auto Day/Night", settings.enableDayNightLighting, leftX, startY, colWidth, 20, value => {
-            settings.enableDayNightLighting = value;
-            updateLighting();
-        }));
-        this.buttonList.push(new GuiSliderButton("Ambient Light", settings.ambientIntensity * 10, 0, 50, leftX, startY + spacing, colWidth, 20, value => {
-            settings.ambientIntensity = value / 10;
-            updateLighting();
-        }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+        // --- Tabs Row ---
+        let tabWidth = 100;
+        let tabStartX = this.width / 2 - (tabWidth * 3 + 10) / 2;
 
-        this.buttonList.push(new GuiSliderButton("Sun Light", settings.sunIntensity * 10, 0, 50, leftX, startY + spacing * 2, colWidth, 20, value => {
-            settings.sunIntensity = value / 10;
-            updateLighting();
-        }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+        let btnGlobal = new GuiButton("Global", tabStartX, startY, tabWidth, 20, () => {
+            this.currentTab = "GLOBAL";
+            this.rebuildButtons();
+        });
+        btnGlobal.enabled = this.currentTab !== "GLOBAL";
+        this.buttonList.push(btnGlobal);
 
-        this.buttonList.push(new GuiSwitchButton("Sun Shadow", settings.sunCastShadow, leftX, startY + spacing * 3, colWidth, 20, value => {
-            settings.sunCastShadow = value;
-            updateLighting();
-        }));
+        let btnSunMoon = new GuiButton("Sun & Moon", tabStartX + tabWidth + 5, startY, tabWidth, 20, () => {
+            this.currentTab = "SUN_MOON";
+            this.rebuildButtons();
+        });
+        btnSunMoon.enabled = this.currentTab !== "SUN_MOON";
+        this.buttonList.push(btnSunMoon);
 
-        this.buttonList.push(new GuiSwitchButton("Sun Helper", settings.showSunLightHelper, leftX, startY + spacing * 4, colWidth, 20, value => {
-            settings.showSunLightHelper = value;
-            updateLighting();
-        }));
+        let btnBlocks = new GuiButton("Blocks", tabStartX + (tabWidth + 5) * 2, startY, tabWidth, 20, () => {
+            this.currentTab = "BLOCKS";
+            this.rebuildButtons();
+        });
+        btnBlocks.enabled = this.currentTab !== "BLOCKS";
+        this.buttonList.push(btnBlocks);
 
-        this.buttonList.push(new GuiSliderButton("Moon Light", settings.moonIntensity * 10, 0, 50, leftX, startY + spacing * 5, colWidth, 20, value => {
-            settings.moonIntensity = value / 10;
-            updateLighting();
-        }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+        // --- Options Area ---
+        let optionStartY = startY + spacing * 2;
 
-        this.buttonList.push(new GuiSwitchButton("Moon Shadow", settings.moonCastShadow, leftX, startY + spacing * 6, colWidth, 20, value => {
-            settings.moonCastShadow = value;
-            updateLighting();
-        }));
+        if (this.currentTab === "GLOBAL") {
+            this.buttonList.push(new GuiSwitchButton("Auto Day/Night", settings.enableDayNightLighting, centerX, optionStartY, 200, 20, value => {
+                settings.enableDayNightLighting = value;
+                updateLighting();
+            }));
+            this.buttonList.push(new GuiSliderButton("Ambient Light", settings.ambientIntensity * 10, 0, 50, centerX, optionStartY + spacing, 200, 20, value => {
+                settings.ambientIntensity = value / 10;
+                updateLighting();
+            }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
 
-        this.buttonList.push(new GuiSwitchButton("Moon Helper", settings.showMoonLightHelper, leftX, startY + spacing * 7, colWidth, 20, value => {
-            settings.showMoonLightHelper = value;
-            updateLighting();
-        }));
+        } else if (this.currentTab === "SUN_MOON") {
+            // Sun (Left)
+            this.buttonList.push(new GuiSliderButton("Sun Light", settings.sunIntensity * 10, 0, 50, leftX, optionStartY, colWidth, 20, value => {
+                settings.sunIntensity = value / 10;
+                updateLighting();
+            }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+            this.buttonList.push(new GuiSwitchButton("Sun Shadow", settings.sunCastShadow, leftX, optionStartY + spacing, colWidth, 20, value => {
+                settings.sunCastShadow = value;
+                updateLighting();
+            }));
+            this.buttonList.push(new GuiSwitchButton("Sun Helper", settings.showSunLightHelper, leftX, optionStartY + spacing * 2, colWidth, 20, value => {
+                settings.showSunLightHelper = value;
+                updateLighting();
+            }));
 
-        // --- Column 2 (Right: Torches & Spotlights) ---
-        this.buttonList.push(new GuiSliderButton("Torch Light", settings.torchIntensity * 10, 0, 100, rightX, startY, colWidth, 20, value => {
-            settings.torchIntensity = value / 10;
-            updateLighting();
-        }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+            // Moon (Right)
+            this.buttonList.push(new GuiSliderButton("Moon Light", settings.moonIntensity * 10, 0, 50, rightX, optionStartY, colWidth, 20, value => {
+                settings.moonIntensity = value / 10;
+                updateLighting();
+            }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+            this.buttonList.push(new GuiSwitchButton("Moon Shadow", settings.moonCastShadow, rightX, optionStartY + spacing, colWidth, 20, value => {
+                settings.moonCastShadow = value;
+                updateLighting();
+            }));
+            this.buttonList.push(new GuiSwitchButton("Moon Helper", settings.showMoonLightHelper, rightX, optionStartY + spacing * 2, colWidth, 20, value => {
+                settings.showMoonLightHelper = value;
+                updateLighting();
+            }));
 
-        this.buttonList.push(new GuiSliderButton("Torch Dist", settings.torchDistance, 5, 50, rightX, startY + spacing, colWidth, 20, value => {
-            settings.torchDistance = value;
-            updateLighting();
-        }));
+        } else if (this.currentTab === "BLOCKS") {
+            // Torches (Left)
+            this.buttonList.push(new GuiSliderButton("Torch Light", settings.torchIntensity * 10, 0, 100, leftX, optionStartY, colWidth, 20, value => {
+                settings.torchIntensity = value / 10;
+                updateLighting();
+            }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+            this.buttonList.push(new GuiSliderButton("Torch Dist", settings.torchDistance, 5, 50, leftX, optionStartY + spacing, colWidth, 20, value => {
+                settings.torchDistance = value;
+                updateLighting();
+            }));
+            this.buttonList.push(new GuiSwitchButton("Torch Shadow", settings.torchCastShadow, leftX, optionStartY + spacing * 2, colWidth, 20, value => {
+                settings.torchCastShadow = value;
+                updateLighting();
+            }));
 
-        this.buttonList.push(new GuiSwitchButton("Torch Shadow", settings.torchCastShadow, rightX, startY + spacing * 2, colWidth, 20, value => {
-            settings.torchCastShadow = value;
-            updateLighting();
-        }));
-
-        this.buttonList.push(new GuiSliderButton("SpotLight", settings.spotLightIntensity * 10, 0, 200, rightX, startY + spacing * 3, colWidth, 20, value => {
-            settings.spotLightIntensity = value / 10;
-            updateLighting();
-        }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
-
-        this.buttonList.push(new GuiSliderButton("Spot Angle", settings.spotLightAngle, 0, 90, rightX, startY + spacing * 4, colWidth, 20, value => {
-            settings.spotLightAngle = value;
-            updateLighting();
-        }).setDisplayNameBuilder((name, value) => name + ": " + value + ""));
-
-        this.buttonList.push(new GuiSwitchButton("Spot Shadow", settings.spotLightCastShadow, rightX, startY + spacing * 5, colWidth, 20, value => {
-            settings.spotLightCastShadow = value;
-            updateLighting();
-        }));
-
-        this.buttonList.push(new GuiSwitchButton("Spot Helper", settings.showSpotLightHelper, rightX, startY + spacing * 6, colWidth, 20, value => {
-            settings.showSpotLightHelper = value;
-            updateLighting();
-        }));
+            // Spotlights (Right)
+            this.buttonList.push(new GuiSliderButton("SpotLight", settings.spotLightIntensity * 10, 0, 200, rightX, optionStartY, colWidth, 20, value => {
+                settings.spotLightIntensity = value / 10;
+                updateLighting();
+            }).setDisplayNameBuilder((name, value) => name + ": " + (value / 10).toFixed(1)));
+            this.buttonList.push(new GuiSliderButton("Spot Angle", settings.spotLightAngle, 0, 90, rightX, optionStartY + spacing, colWidth, 20, value => {
+                settings.spotLightAngle = value;
+                updateLighting();
+            }).setDisplayNameBuilder((name, value) => name + ": " + value + ""));
+            this.buttonList.push(new GuiSwitchButton("Spot Shadow", settings.spotLightCastShadow, rightX, optionStartY + spacing * 2, colWidth, 20, value => {
+                settings.spotLightCastShadow = value;
+                updateLighting();
+            }));
+            this.buttonList.push(new GuiSwitchButton("Spot Helper", settings.showSpotLightHelper, rightX, optionStartY + spacing * 3, colWidth, 20, value => {
+                settings.showSpotLightHelper = value;
+                updateLighting();
+            }));
+        }
 
         // Done button at bottom
-        let maxSpacing = 7;
-        this.buttonList.push(new GuiButton("Done", this.width / 2 - 100, startY + spacing * maxSpacing + 10, 200, 20, () => {
+        this.buttonList.push(new GuiButton("Done", centerX, this.height - 40, 200, 20, () => {
             this.minecraft.displayScreen(this.previousScreen);
         }));
     }
--- source/src/js/net/minecraft/client/render/WorldRenderer.js
+++ source/src/js/net/minecraft/client/render/WorldRenderer.js
@@ -619,8 +619,8 @@ export default class WorldRenderer {
 
         if (this.minecraft.settings && this.minecraft.settings.enableDayNightLighting) {
             this.sunLight.intensity = brightness * 1.2;
-            this.moonLight.intensity = (1.0 - brightness) * this.minecraft.settings.moonIntensity;
-            this.ambientLight.intensity = 0.15 + brightness * 0.25;
+            this.moonLight.intensity = (1.0 - brightness) * 0.4; // Tự động set max là 0.4 (thay vì 1.2 như mặt trời)
+            this.ambientLight.intensity = 0.1 + brightness * 0.25;
         }
         this.overlayAmbientLight.intensity = 0.1 + brightness * 0.15;
 
